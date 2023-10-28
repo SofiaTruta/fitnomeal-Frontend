01a6d6ba49a4da6878f2558e5894bdffce41b9d7
sofiatruta <sofiatruta@gmail.com>
2023-10-28 15:00:37 +0100

    nextauth and google basic setup. not connected to db yet

--- app/welcome/page.jsx
+++ app/welcome/page.jsx
@@ -1,5 +1,12 @@
+'use client'
+import { signIn, useSession } from 'next-auth/react'
+import GoogleButton from 'react-google-button'
+
 export default function Welcome() {
   return (
-    <div>Welcome</div>
+    <>
+      <div>Welcome</div>
+      <GoogleButton onClick={() => signIn('google')} />
+    </>
   )
 }
