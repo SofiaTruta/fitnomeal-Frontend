db52d4ae68c65da2731bd28488039d7ee0cf23ab
OnlyOneLz <elliotrobertlewis@hotmail.com>
2023-11-01 16:10:56 +0000

    history page

--- app/goals/page.jsx
+++ app/goals/page.jsx
@@ -1,33 +1,42 @@
 'use client'
-import { useSession } from "next-auth/react";
+import { useSession} from "next-auth/react";
 import NavBar from "@/components/navbar/NavBar";
+import { useEffect, useState } from "react";
 
 export default function Goals() {
   const { data: session, status } = useSession()
+  const [userData, setUserData] = useState(null);
 
-  async function fetchUserData() {
-    try {
-      const response = await fetch(`${WORKOUT_DATA}workout-history/find/${email}`, {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
+  const WORKOUT_DATA = process.env.NEXT_PUBLIC_BACKEND_CONNECTION
 
-      const result = await response.json();
-      setUserData(result); // Set user data in state
-    } catch (error) {
-      console.log("User not found", error);
+  useEffect(() => {
+    if (session) {
+      const email = session.user.email;
+      async function fetchUserData() {
+        try {
+          const response = await fetch(`${WORKOUT_DATA}/workout-history/history/${email}`, {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+            },
+          });
+
+          const result = await response.json();
+          console.log(result);
+          setUserData(result); // Set user data in state
+        } catch (error) {
+          console.log("User not found", error);
+        }
+      }
+
+      fetchUserData(); // Call the function to fetch user data
     }
-  }
+  }, [session])
 
-  fetchUserData(); // Call the function to fetch user data
   return (
     <div>
+      <NavBar />
       <h1>Goals page</h1>
-      <NavBar/>
     </div>
-    
-
   )
 }
--- app/history/page.jsx
+++ app/history/page.jsx
@@ -1,19 +1,60 @@
-// import Link from 'next/link'; // Import Link from Next.js
-// import WorkoutHistory from '../models/WorkoutHistory'; // Import your Mongoose model
+'use client'
+import { useSession } from "next-auth/react";
+import NavBar from "@/components/navbar/NavBar";
+import { useEffect, useState } from "react";
 
-function WorkoutHistoryPage({ workoutHistory }) {
+export default function WorkoutHistoryPage() {
+  const { data: session, status } = useSession();
+  const [workoutHistory, setWorkoutHistory] = useState(null);
+
+  const WORKOUT_DATA = process.env.NEXT_PUBLIC_BACKEND_CONNECTION;
+
+  useEffect(() => {
+    if (session) {
+      const email = session.user.email;
+      async function fetchWorkoutHistory() {
+        try {
+          const response = await fetch(`${WORKOUT_DATA}/workout-history/history/${email}`, {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+            },
+          });
+
+          const result = await response.json();
+          console.log(result);
+          setWorkoutHistory(result); // Set user data in state
+        } catch (error) {
+          console.log("User not found", error);
+        }
+      }
+
+      fetchWorkoutHistory(); // Call the function to fetch user data
+    }
+  }, [session]);
+
+
+// const Workouts = workoutHistory.map(workout => (
+//   <div key={index}>
+//     <h2>{workout.name}</h2>
+//     <img src={workout.gifUrl} alt="Workout GIF" />
+//   </div>
+// ));
+
   return (
     <div>
-      <h1>Workout History</h1>
-      {/* <ul>
-        {workoutHistory.map((entry) => (
-          <li key={entry._id}>
-            <Link href={`/workout-details/${entry._id}`}>
-              <a>{entry.date}</a>
-            </Link>
-          </li>
-        ))}
-      </ul> */}
+      <NavBar workout={workoutHistory} />
+      <h1>WorkoutHistoryPage</h1>
+      {workoutHistory ? (
+        workoutHistory.map((workout, index) => (
+          <div key={index}>
+            <p>Date: {new Date(workout.date).toLocaleDateString()}</p>
+            <p>Status: {workout.status}</p>
+          </div>
+        ))
+      ) : (
+        <p>Loading workout history...</p>
+      )}
     </div>
   );
 }
