ee4447803858a8e2ef6146d7182ed9844a5bd93e
OnlyOneLz <elliotrobertlewis@hotmail.com>
2023-11-01 12:57:09 +0000

    thankyoumodal

--- app/goals/page.jsx
+++ app/goals/page.jsx
@@ -1,11 +1,33 @@
 'use client'
 import { useSession } from "next-auth/react";
+import NavBar from "@/components/navbar/NavBar";
 
 export default function Goals() {
   const { data: session, status } = useSession()
-  console.log(session);
+
+  async function fetchUserData() {
+    try {
+      const response = await fetch(`${WORKOUT_DATA}workout-history/find/${email}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      const result = await response.json();
+      setUserData(result); // Set user data in state
+    } catch (error) {
+      console.log("User not found", error);
+    }
+  }
+
+  fetchUserData(); // Call the function to fetch user data
   return (
-    <div>Goals Page</div>
+    <div>
+      <h1>Goals page</h1>
+      <NavBar/>
+    </div>
+    
 
   )
 }
