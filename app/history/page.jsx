650e04d073a2ccd20b92ab242cde66ab39ea071b
OnlyOneLz <elliotrobertlewis@hotmail.com>
2023-11-01 12:32:20 +0000

    thankyoumodal

--- app/history/page.jsx
+++ app/history/page.jsx
@@ -1,11 +1,11 @@
-import Link from 'next/link'; // Import Link from Next.js
-import WorkoutHistory from '../models/WorkoutHistory'; // Import your Mongoose model
+// import Link from 'next/link'; // Import Link from Next.js
+// import WorkoutHistory from '../models/WorkoutHistory'; // Import your Mongoose model
 
 function WorkoutHistoryPage({ workoutHistory }) {
   return (
     <div>
       <h1>Workout History</h1>
-      <ul>
+      {/* <ul>
         {workoutHistory.map((entry) => (
           <li key={entry._id}>
             <Link href={`/workout-details/${entry._id}`}>
@@ -13,7 +13,7 @@ function WorkoutHistoryPage({ workoutHistory }) {
             </Link>
           </li>
         ))}
-      </ul>
+      </ul> */}
     </div>
   );
 }
