f5047bad610f3908d804bc749ab3897c38099727
OnlyOneLz <elliotrobertlewis@hotmail.com>
2023-11-01 09:32:17 +0000

    firstLoginpage, daily workout, main app

--- app/page.jsx
+++ app/page.jsx
@@ -10,8 +10,8 @@ export default function Home() {
 
   //check if user is signed in already
   const { data: session, status } = useSession()
- const email = session?.user?.email
- 
+  const email = session?.user?.email
+
   const router = useRouter()
 
   const WORKOUT_DATA = process.env.NEXT_PUBLIC_BACKEND_CONNECTION
@@ -20,7 +20,6 @@ export default function Home() {
     //!Fetch for user data so we can tell if its the first time they have logged im
     //! if so router.push to the additional signup details page 
     async function userData() {
-      console.log(email);
       try {
         const response = await fetch(`${WORKOUT_DATA}/users/find/${email}`, {
           method: 'GET',
@@ -28,32 +27,31 @@ export default function Home() {
             "Content-Type": "application/json"
           },
         });
-  
+
         const result = await response.json();
-        console.log(result);
         if (result.firstLoggin === 'true') router.push('/welcome/first-login')
       } catch (error) {
         console.log('user not found');
       }
     }
     userData() // Call the function to fetch user data
-   
+
 
     if (status === "unauthenticated") router.push('/welcome')
-    
 
-  }, [session]); 
-  
-  
+
+  }, [session]);
+
+
   //state to pass down to props
   const [choice, setChoice] = useState('Full Body')
 
 
-  
+
   return (
     <>
       {status === "authenticated" ? (
-        <> 
+        <>
           <NavBar />
           <h1>Home</h1>
           <NewWorkoutModal choice={choice} setChoice={setChoice} />
