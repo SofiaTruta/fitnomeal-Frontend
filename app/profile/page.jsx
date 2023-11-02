b7f4a726b6026d96255d3e7f41669c6011613b40
Aisha Ayinde <aishaayinde@Aishas-MacBook-Air.local>
2023-11-02 15:36:14 +0000

    styled daily workouts

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -93,7 +93,7 @@ const closeModal = () => {
   
 
   return (
-    <div className="bg-purple-50 min-h-screen text-color-dark">
+    <div className="bg-purple-100 min-h-screen text-color-dark">
     <NavBar />
     <div className="p-4">
       <h1 className="text-4xl font-bold text-center mb-4">Profile Page</h1>
